30458b48230663b1231805cd3df9dda918497bde
shanzhashayu <2191706467@qq.com>
2026-04-10 20:28:09 +0800

    第三次提交代码

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
+"""
 print("hello world")
 print("hello world")
-
-
-
+"""
+name = "传智播客"
+stock_price = 19.99
+stock_code = "003032"
+stock_price_daily_growth_factor = 1.2
+growth_days = 7
+print(f"公司:{name},股票代码:{stock_code},当前股价:{stock_price}")
+print("每日增长指数%.2f，""经过%d天后的价格为%.2f"%(stock_price_daily_growth_factor,
+growth_days,
+stock_price*1.2*1.2*1.2*1.2*1.2*1.2*1.2))
 
 
 
