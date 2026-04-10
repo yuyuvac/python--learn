539d2178d6dea5c9a1ad1e191291322380d710a9
shanzhashayu <2191706467@qq.com>
2026-04-10 21:07:09 +0800

    第四次提交代码

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 print("hello world")
 print("hello world")
 """
+"""
 name = "传智播客"
 stock_price = 19.99
 stock_code = "003032"
@@ -11,8 +12,10 @@ print(f"公司:{name},股票代码:{stock_code},当前股价:{stock_price}")
 print("每日增长指数%.2f，""经过%d天后的价格为%.2f"%(stock_price_daily_growth_factor,
 growth_days,
 stock_price*1.2*1.2*1.2*1.2*1.2*1.2*1.2))
-
-
+"""
+user_name = input("请输入用户名")
+user_type = input("请输入用户类型")
+print(f"您好{user_name},尊贵的{user_type}用户,欢迎你的光临")
 
 
 
