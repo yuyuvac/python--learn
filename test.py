652e686053d065bf96d16cf81f2f5971d040c8f3
shanzhashayu <2191706467@qq.com>
2026-04-11 21:52:11 +0800

    第五次提交代码

--- test.py
+++ test.py
@@ -13,10 +13,38 @@ print("每日增长指数%.2f，""经过%d天后的价格为%.2f"%(stock_price_d
 growth_days,
 stock_price*1.2*1.2*1.2*1.2*1.2*1.2*1.2))
 """
+"""
 user_name = input("请输入用户名")
 user_type = input("请输入用户类型")
 print(f"您好{user_name},尊贵的{user_type}用户,欢迎你的光临")
-
-
-
-
+"""
+import random
+num = random.randint (1,10)
+print("欢迎来到猜数字游戏，你有三次机会")
+guess = int(input())
+if guess == num:
+    print("恭喜你你猜中了")
+else:
+    if guess > num:
+        print("你猜的数字大了")
+    else :
+        print("你猜的数字小了")
+    print("你还有两次机会") 
+    guess = int(input())
+    if guess == num:
+        print("恭喜你猜中了") 
+    else:
+        if guess> num :
+            print("你猜的数字大了")
+        else :
+            print("你猜的数字小了")
+        print("你还有一次机会")
+        guess = int(input())
+        if guess == num:
+            print("恭喜你猜中了")
+        else:
+            if guess > num:
+                print("猜的大了")
+            else:
+                print("猜的小了")
+            print(f"你失败了正确数字是{num}，选择重新开始")
